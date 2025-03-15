160dc33a5d7a394cbd92cad31180352670fd05a3
Dariia  web2025-1 <dashadidyk41@gmail.com>
2025-03-15 18:53:39 +0200

    Додано зміни в index.js

--- lab-3/index.js
+++ lab-3/index.js
@@ -24,12 +24,24 @@ if (!options.input) {
 }
 
 const data = fs.readFileSync(options.input, "utf-8");
+let jsonData;
 
+try {
+  jsonData = JSON.parse(data);
+} catch (err) {
+  console.error("Invalid JSON format");
+  process.exit(1);
+}
+const exchangeRates = jsonData.map(entry => {
+	const date = entry.exchangedate;
+	const rate = entry.rate
+	return `${date}:${rate}`;
+});
 if (options.display) {
-  console.log("JSON вміст:", JSON.parse(data));
+  console.log(exchangeRates.join("\n"));
 }
 
 if (options.output) {
-  fs.writeFileSync(options.output, data);
+  fs.writeFileSync(options.output, exchangeRates.join("\n"));
   console.log(`Результат збережено у файл: ${options.output}`);
 }
